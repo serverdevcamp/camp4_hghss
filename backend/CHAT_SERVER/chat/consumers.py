57fb32a107ef9b532693df935f8b25ed5d5d7486
naye0ng <nayeong_e@naver.com>
2020-01-25 16:49:43 +0900

    Add django-channers and redis
    - save chat message in redis
    - add function : return previous chat list on first connection

--- backend/CHAT_SERVER/chat/consumers.py
+++ backend/CHAT_SERVER/chat/consumers.py
@@ -1,4 +1,5 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
+import redis
 import json
 # https://ssungkang.tistory.com/entry/Django-Channels-%EB%B9%84%EB%8F%99%EA%B8%B0%EC%A0%81-%EC%B1%84%ED%8C%85-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0-WebSocket-3?category=320582
 class ChatConsumer(AsyncWebsocketConsumer) :
@@ -16,6 +17,16 @@ class ChatConsumer(AsyncWebsocketConsumer) :
         
         await self.accept()
 
+
+        # TDOD : 최적화 필요
+        r = redis.Redis(charset="utf-8", decode_responses=True)
+        chat_list = r.lrange(self.company_chat,0,-1)
+        
+
+        await self.send(text_data = json.dumps({
+            'chat_list' : chat_list
+        }))
+
     async def disconnect(self, close_code) :
         await self.channel_layer.group_discard(
             self.company_chat,
@@ -24,21 +35,28 @@ class ChatConsumer(AsyncWebsocketConsumer) :
 
     async def receive(self, text_data) :
         text_data_json = json.loads(text_data)
+        nickname = text_data_json['nickname']
         message = text_data_json['message']
 
-        # 채팅방 모두에게 메시지 보내기
+        r = redis.Redis()
+        r.lpush(self.company_chat, text_data)
+
         await self.channel_layer.group_send(
             self.company_chat,
             {
                 'type' : 'chat_message',
-                'message' : message
+                'message' : message,
+                'nickname' : nickname
             }
         )
     
     # 채팅방에서 메시지 receive
     async def chat_message(self, event) :
         message = event['message']
+        nickname = event['nickname']
+        
         # 소켓에게 메시지 전달
         await self.send(text_data = json.dumps({
-            'message':message
+            'message': message,
+            'nickname' : nickname
         }))
--- backend/CHAT_SERVER/chat/routing.py
+++ backend/CHAT_SERVER/chat/routing.py
@@ -5,4 +5,21 @@ websocket_urlpatterns = [
     url(r"^chat/(?P<company>\w+)/$", consumers.ChatConsumer)
 ]
 
-# var socket = new WebSocket('ws://' + 'localhost:8080' + '/chat/company1/');
+"""
+var socket = new WebSocket('ws://' + 'localhost:8080' + '/chat/company1/');
+var chat = []
+
+// 데이터 전송
+socket.onopen = function (event) {
+    // 소켓열리고 실행
+    var msg = {message:"메시지 테스트@@!!", nickname: "헤온 쥐잡이뱀"};
+    socket.send(JSON.stringify(msg)); 
+};
+
+// 데이터 수신
+socket.onmessage = function(event) {
+    var msg = JSON.parse(event.data);
+    console.log(msg)
+    chat.push(msg)
+}
+"""
