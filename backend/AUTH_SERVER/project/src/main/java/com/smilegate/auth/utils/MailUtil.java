ce7c297d3e34e9479f99a2b997b859b17d8f4a46
yun <ybchae0801@naver.com>
2020-02-28 06:11:53 +0900

    Modify signup callback url

--- backend/AUTH_SERVER/project/src/main/java/com/smilegate/auth/utils/MailUtil.java
+++ backend/AUTH_SERVER/project/src/main/java/com/smilegate/auth/utils/MailUtil.java
@@ -21,6 +21,12 @@ public class MailUtil {
     @Value("${spring.mail.password}")
     private String fromEmailpassword;//발신자의 이메일 패스워드
 
+    @Value("${mail.signup.callback-url}")
+    private String signupCallbackUrl;
+
+    @Value("${mail.password.callback-url}")
+    private String passwordCallbackUrl;
+
     private Session session;
 
     public MailUtil() {
@@ -49,7 +55,7 @@ public class MailUtil {
 
             message.setContent(
                     "<h1>[이메일 인증]</h1> <p>아래 링크를 클릭하시면 이메일 인증이 완료됩니다.</p> " +
-                    "<a href='http://13.209.238.123:8888/users/signup/confirm?key=" + key + "' target='_blenk'>이메일 인증 확인</a>"
+                    "<a href='"+signupCallbackUrl+"/users/signup/confirm?key=" + key + "' target='_blenk'>이메일 인증 확인</a>"
                     , "text/html;charset=euc-kr"
             );
 
@@ -72,7 +78,7 @@ public class MailUtil {
             message.setSubject("[합격하소서] 비밀번호 찾기 전송"); //메일 제목을 입력
 
             message.setContent("<h1>[비밀번호 변경]</h1> <p>아래 링크를 클릭하시면 비밀번호를 변경하실 수 있습니다.</p> " +
-                    "<a href='http://localhost:8080/password/change?key="
+                    "<a href='"+passwordCallbackUrl+"/password/change?key="
                     +key +"' target='_blenk'>비밀번호 변경하기</a>","text/html;charset=euc-kr");
 
             // send the message
